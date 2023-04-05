85e19ac5b6e45c696cbf8b7eb6b2140060ab517b
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-05 07:21:03 +0545

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Routes, Route, Navigate} from "react-router-dom";
 import { Home } from "./components/Home";
 import { ProductList } from './components/ProductList';
 import { ProductDetail } from './components/ProductDetail';
-import { Contact } from './components/Contact';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Admin } from './components/Admin';
 import { PageNotFound } from "./components/PageNotFound";
+import { Contact } from './components/Contact';
+import { ContactIn } from "./components/ContactIn";
+import { ContactEu } from "./components/ContactEu";
+import { ContactUs } from "./components/ContactUs";
+import { ContactOther } from "./components/ContactOther";
+
 
 function App() {
   const user = true;
@@ -20,7 +25,14 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="products" element={<ProductList />} />
           <Route path="products/:id" element={<ProductDetail />} />
-          <Route path="contact" element={<Contact />} />
+
+          <Route path="contact" element={<Contact />} >
+            <Route path="in" element={<ContactIn />} />
+            <Route path="eu" element={<ContactEu />} />
+            <Route path="us" element={<ContactUs />} />
+            <Route path="*" element={<ContactOther />} />
+          </Route>  
+
           <Route path="/dipesh" element={<Navigate to="/" />} />
           <Route path="/admin" element={ user ? <Admin /> : <Navigate to="/" />} />
           <Route path="*" element={<PageNotFound />} />
