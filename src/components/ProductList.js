3ccf5e400030c9ec03b762a74b0d89674a33ecd0
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-05 06:16:57 +0545

    uselocation added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,12 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams , useLocation} from "react-router-dom";
 
 export const ProductList = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("q"));
+  // console.log(searchParams.get("keyword"));
+
+  const location = useLocation();
+  console.log(location);
+
 
   return (
     <div className='component' >ProductList</div>
